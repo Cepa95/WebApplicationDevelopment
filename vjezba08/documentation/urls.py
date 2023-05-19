e2a597004e141e446c61f1de684427803f18f80b
Cepa95 <ceprnic.josip@gmail.com>
2023-05-19 02:53:01 +0200

    css

--- vjezba08/documentation/urls.py
+++ vjezba08/documentation/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from vjezba8.views import add_user
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('add_user/', add_user, name='add_user'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
